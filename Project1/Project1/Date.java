6bcecb75e4d132e163b6b3fef00ace0ef7450972
Sebe9 <sebastian.hanna03@gmail.com>
2023-09-30 23:25:23 -0400

    started working on the isValid method

--- Project1/Project1/Date.java
+++ Project1/Project1/Date.java
@@ -3,8 +3,33 @@ public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
-    //check if the date is a valid calendar date
+
+    //will move these to another class at some point (I think we are supposed to make an enum class)
+    public static final int MIN_MONTH = 1; 
+    public static final int MAX_MONTH = 12;
+    public static final int MIN_YEAR = 0;
+    public static final int MIN_DAY = 0;
+    public static final int MAX_DAY = 31;
+
+    //check if the date is a valid calendar date 
     public boolean isValid(){
-        return false;
-    } 
+
+        if (month < MIN_MONTH || month > MAX_MONTH ){
+            return false; }
+        else if (year < MIN_YEAR){
+            return false; }
+        else if (day < MIN_DAY || day > MAX_DAY){
+            return false; }
+       
+
+        
+        
+        return true;
+    }
+
+    public Date(int year, int month, int day){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
 }
