926e3d216f42e8e9bc80dbe8781a0f1833b9ebfc
matchamatt <0matthew.chan0@gmail.com>
2023-10-01 17:06:20 -0400

    finished enum classes

--- Project1/Project1/Location.java
+++ Project1/Project1/Location.java
@@ -1,4 +1,19 @@
 package project1;
-public class Location {
-    //hi
+public enum Location {
+    HLL114("Hill Center, Busch"), 
+    ARC103("Allison Road Classroom, Busch"),
+    BE_AUD("Beck Hall, Livingston"),
+    TIL232("Tillet Hall, Livingston"),
+    AB2225("Academic Building, College Avenue"),
+    MU302("Murray Hall, College Avenue");
+
+    private String roomLocation;
+
+    Location(String roomLocation){
+        this.roomLocation = roomLocation;
+    }
+
+    public String getLocation() {
+        return roomLocation;
+    }
 }
--- Project1/Project1/Timeslot.java
+++ Project1/Project1/Timeslot.java
@@ -1,4 +1,14 @@
 package project1;
-public class Timeslot {
+public enum Timeslot {
+    morning("10:30am"), afternoon("2:00pm"), evening("6:30pm");
     
+    private String time;
+
+    Timeslot(String time){
+        this.time = time;
+    }
+
+    public String getTime(){
+        return time;
+    }
 }
