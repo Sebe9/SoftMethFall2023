3e4bf8c7ee94eb334052de3e11599dc8aa1d314f
Sebe9 <sebastian.hanna03@gmail.com>
2023-09-30 21:58:35 -0400

    added classes needed for Event class

---
+++ src/Event.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+
+public class Event implements Comparable<Event> {
+    private Date date; //the event date
+    private Timeslot startTime; //the starting time
+    private Location location;
+    private Contact contact; //include the department name and email
+    private int duration; //in minutes
+    //Compares two events based off of their date. If the dates are the same it will compare timeslots
+    @Override public int compareTo(Event event1, Event event2){
+
+    }
+    //Returns a textual representation of an event
+    @Override public String toString(Event event1){
+
+    }
+    //Returns true if two dates, timeslots, and locations are equal
+    @Override public bool equals(){
+
+    }
+
+    }
